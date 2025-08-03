34419cadaf7c32706622df00ac48866685cf833b
felipe j. Duarte da Silva <felipej.d.silvadf@gmail.com>
2025-08-03 17:23:06 -0300

    terminando os desafios de estruturas de dados

--- desafios.py
+++ desafios.py
@@ -53,8 +53,94 @@ glicemia = [129, 82, 60, 97, 101, 65, 62, 167, 87, 53, 58, 92, 66, 120, 109, 62,
 
 glicemia = [129, 82, 60, 97, 101, 65, 62, 167, 87, 53, 58, 92, 66, 120, 109, 62, 86, 96, 103, 88, 155, 52, 89, 73]
 
-glicemias_rotuladas = [ (rotulo, valor) for valor in glicemia if (rotulo := 'Hipoglicemia' if valor <= 70 else
-                        'Normal' if 70 < valor <= 99 else
-                        'Alterada' if 100 <= valor <= 125 else
-                        'Diabetes')]
+glicemias_rotuladas = [
+    ( 
+        'Hipoglicemia' if valor <= 70 else
+        'Normal'       if valor <= 99 else
+        'Alterada'     if valor <= 125 else
+        'Diabetes',
+        valor
+    ) 
+    for valor in glicemia
+]
+
 print(glicemias_rotuladas) 
+
+'''
+Um e-commerce possui as informações de id de venda, quantidade vendida e preço do produto divididos nas seguintes listas:
+
+id = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+quantidade = [15, 12, 1, 15, 2, 11, 2, 12, 2, 4]
+preco = [93.0, 102.0, 18.0, 41.0, 122.0, 14.0, 71.0, 48.0, 14.0, 144.0]
+
+O e-commerce precisa estruturar esses dados em uma tabela contendo o valor total da venda, que é obtida multiplicando a quantidade pelo preço unitário. Além disso, a tabela precisa conter um cabeçalho indicando as colunas: 'id', 'quantidade', 'preco' e 'total'.
+
+Crie uma lista de tuplas em que cada tupla tenha id, quantidade, preço e valor total, na qual a primeira tupla é o cabeçalho da tabela.
+
+("id", "quantidade", "preco", "valor_total")
+
+'''
+
+id = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+quantidade = [15, 12, 1, 15, 2, 11, 2, 12, 2, 4]
+preco = [93.0, 102.0, 18.0, 41.0, 122.0, 14.0, 71.0, 48.0, 14.0, 144.0]
+
+tabela = [('id', 'quantidade', 'preco', 'total')]
+tabela += [(id[i], quantidade[i], preco[i], quantidade[i]*preco[i]) for i in range(len(id))]
+
+'''
+Uma empresa possui filiais espalhadas nos Estados da região Sudeste do Brasil. Em uma das tabelas de cadastro das filiais há uma coluna contendo a informação de qual é o Estado a que pertence: estados = ['SP', 'ES', 'MG', 'MG', 'SP', 'MG', 'ES', 'ES', 'ES', 'SP', 'SP', 'MG', 'ES', 'SP', 'RJ', 'MG', 'RJ', 'SP', 'MG', 'SP', 'ES', 'SP', 'MG'].
+
+A empresa sempre está abrindo novas filiais, de modo que a tabela está constantemente recebendo novos registros e o gestor gostaria de possuir a informação atualizada da quantidade de filiais em cada Estado.
+
+A partir da coluna com a informação dos Estados, crie um dicionário usando dict comprehension com a chave sendo o nome de um Estado e o valor sendo a contagem de vezes em que o Estado aparece na lista.
+
+Dica: Você pode fazer um passo intermediário para gerar uma lista de listas em que cada uma das listas possui o nome de apenas um Estado com valores repetidos.
+
+'''
+
+estados = ['SP', 'ES', 'MG', 'MG', 'SP', 'MG', 'ES', 'ES', 'ES', 'SP', 'SP', 'MG', 'ES', 'SP', 'RJ', 'MG', 'RJ', 'SP', 'MG', 'SP', 'ES', 'SP', 'MG']
+
+estados_unicos = list(set(estados))
+
+lista_de_listas = []
+for estado in estados_unicos:
+    lista = [uf for uf in estados if uf == estado]
+    lista_de_listas.append(lista)
+print(lista_de_listas)
+
+
+contagem_valores = {estados_unicos[i]: len(lista_de_listas[i]) for i in range(len(estados_unicos))}
+print(contagem_valores)
+
+
+
+'''
+Nessa mesma tabela de cadastro de filiais, há uma coluna com as informações da quantidade de pessoas colaboradoras e o(a) gestor(a) gostaria de ter um agrupamento da soma dessas pessoas para cada estado. As informações contidas na tabela são:
+
+
+funcionarios = [('SP', 16), ('ES', 8), ('MG', 9), ('MG', 6), ('SP', 10), ('MG', 4), ('ES',9), ('ES', 7), ('ES', 12), ('SP', 7), ('SP', 11), ('MG',8), ('ES',8), ('SP',9), ('RJ', 13), ('MG', 5), ('RJ', 9), ('SP', 12), ('MG', 10), ('SP', 7), ('ES', 14), ('SP', 10), ('MG', 12)]
+
+A partir da lista de tuplas, crie um dicionário em que as chaves são os nomes dos Estados únicos e os valores são as listas com o número de colaboradores(as) referentes ao Estado. Crie também um dicionário em que as chaves são os nomes dos Estados e os valores são a soma de colaboradores(as) por Estado.
+
+
+'''
+
+
+funcionarios = [('SP', 16), ('ES', 8), ('MG', 9), ('MG', 6), ('SP', 10), ('MG', 4), ('ES',9), ('ES', 7), ('ES', 12), ('SP', 7), ('SP', 11), ('MG',8), ('ES',8), ('SP',9), ('RJ', 13), ('MG', 5), ('RJ', 9), ('SP', 12), ('MG', 10), ('SP', 7), ('ES', 14), ('SP', 10), ('MG', 12)]
+
+
+estados_unicos = list(set([tupla[0] for tupla in funcionarios]))
+
+lista_de_listas = []
+for estado in estados_unicos:
+    lista = [tupla[1] for tupla in funcionarios if tupla[0] == estado]
+    lista_de_listas.append(lista)
+print(lista_de_listas)
+
+agrupamento_por_estado = {estados_unicos[i]: lista_de_listas[i] for i in range(len(estados_unicos))}
+print(agrupamento_por_estado)
+
+soma_por_estado = {estados_unicos[i]: sum(lista_de_listas[i]) for i in range(len(estados_unicos))}
+print(soma_por_estado)
+
